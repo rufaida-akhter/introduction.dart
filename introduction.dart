5dc9266b06b9dad6925c6969815d668f5ed04812
rufaida-akhter <akhter.akhi21@gmail.com>
2023-09-04 14:23:06 +0600

    dart learning day 2

--- introduction.dart
+++ introduction.dart
@@ -3,9 +3,14 @@ void main(){
   print(a);
   print("hello akhi $a");
   // variables
-String name="rima";
-print(name);
+String name1="rima";
+print(name1);
 int x=12;
 print(x);
+// Dart Printing and String Interpolation
+var name ="pitter";
+var age=24;
+print("my name is $name and i am $age years old");
+
 
 }
