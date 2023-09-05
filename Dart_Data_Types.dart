798e666c5380254693efdc223d461aca14e818b2
rufaida-akhter <akhter.akhi21@gmail.com>
2023-09-05 13:50:27 +0600

    number and int and double

--- Dart_Data_Types.dart
+++ Dart_Data_Types.dart
@@ -1,4 +1,9 @@
 void main()
 {
-
+  // Dart is static typed and types annotations language. Dart can infer a type and types annotations are optional.
+// int marks = 80;
+int marks = 1;
+double pi=3.14;
+double mymark=(marks+marks)*20+pi;
+print("the mark is $marks+$mymark and $pi");
 }
