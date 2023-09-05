3f32bd226cd931fcc92eabb04c7a2170397c2e09
rufaida-akhter <akhter.akhi21@gmail.com>
2023-09-05 13:15:31 +0600

    Dart Data Types

--- introduction.dart
+++ introduction.dart
@@ -1,16 +1,32 @@
-void main(){
-  var a ;
-  print(a);
-  print("hello akhi $a");
-  // variables
-String name1="rima";
-print(name1);
-int x=12;
-print(x);
-// Dart Printing and String Interpolation
-var name ="pitter";
-var age=24;
-print("my name is $name and i am $age years old");
+// void main(){
+//   var a ;
+//   print(a);
+//   print("hello akhi $a");
+//   // variables
+// String name1="rima";
+// print(name1);
+// int x=12;
+// print(x);
+// // Dart Printing and String Interpolation
+// var name ="pitter";
+// var age=24;
+// print("my name is $name and i am $age years old");
 
 
-}
+// }
+
+// void main() {   
+//   var var1 = "hello";   
+//    print(var1);   
+// // }
+void main(){  
+    // This will print the given statement on screen  
+    print("Welcome to JavaTpoint");  
+} 
+//
+// void main(){  
+//     /* This is the example of multi-line comment 
+//     This will print the given statement on screen */  
+      
+//     print("Welcome to JavaTpoint");  
+// }    
